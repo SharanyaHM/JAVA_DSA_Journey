3fc9f1c626061723fcff17f9f2e01e3ac1899ece
SharanyaHM <sharanyahmgowda@gmail.com>
2026-07-12 01:42:49 +0530

    Solution for LC026 Remove Duplicates using Two Pointers

---
+++ 01-Arrays/LC026_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,24 @@
+// LC026 - Remove Duplicates from Sorted Array
+// Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// Time: O(n) | Space: O(1)
+
+class LC026_RemoveDuplicatesFromSortedArray {
+    
+    public int removeDuplicates(int[] nums) {
+        int i, k, size;
+        k = 0;
+        size = nums.length;
+        for (i = 1; i < size; i++) {
+            if (nums[i] != nums[k]) {
+                nums[k + 1] = nums[i];
+                k++;
+            }
+
+        }
+        return k + 1;
+
+    }
+
+
+}
+
