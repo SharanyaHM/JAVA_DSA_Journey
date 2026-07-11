ecbe234f6897e2a0569f9d48545ebaa6927970cc
SharanyaHM <sharanyahmgowda@gmail.com>
2026-07-12 01:44:37 +0530

    Solution for LC027 Remove Element using Two Pointers

---
+++ 01-Arrays/LC027_RemoveElements.java
@@ -0,0 +1,17 @@
+// LC027 - Remove Element
+// Link: https://leetcode.com/problems/remove-element/
+// Time: O(n) | Space: O(1)
+
+class LC027_RemoveElements {
+    public int removeElement(int[] nums, int val) {
+        int i, k;
+        k = 0;
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
