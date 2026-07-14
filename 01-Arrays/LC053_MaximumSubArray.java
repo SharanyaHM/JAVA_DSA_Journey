330e32a32fe5097e7fd0576a4cd61170cef7aa12
SharanyaHM <sharanyahmgowda@gmail.com>
2026-07-14 13:45:17 +0530

    Solution for subarray of maximum sum using Kadane's Algorithm

---
+++ 01-Arrays/LC053_MaximumSubArray.java
@@ -0,0 +1,19 @@
+// LC53 - Maximum Subarray ->Kadane's Algorithm
+// Link: https://leetcode.com/problems/maximum-subarray/
+// Time: O(n) | Space: O(1)
+
+class LC053_MaximumSubArray {
+    public int maxSubArray(int[] nums) {
+        int maxsum = nums[0];
+        int cursum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (cursum <= 0) {
+                cursum = 0;
+            }
+            cursum += nums[i];
+            maxsum = Integer.max(maxsum, cursum);
+        }
+        return maxsum;
+
+    }
+}
