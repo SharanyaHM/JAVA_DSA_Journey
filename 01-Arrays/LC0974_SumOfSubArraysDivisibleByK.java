d53aa551094521a3dd2f830eefc2f56f1478730e
SharanyaHM <sharanyahmgowda@gmail.com>
2026-07-14 13:46:47 +0530

    Solution for number of Subarray's sum divisible by k

---
+++ 01-Arrays/LC0974_SumOfSubArraysDivisibleByK.java
@@ -0,0 +1,28 @@
+// LC974 - Subarray Sums Divisible by K
+// Link: https://leetcode.com/problems/subarray-sums-divisible-by-k/
+// Time: O(n) | Space: O(n)
+
+import java.util.HashMap;
+public class LC0974_SumOfSubArraysDivisibleByK {
+
+    public int subarraysDivByK(int[] nums, int k) {
+        int count = 0;
+        int prefixsum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            prefixsum += nums[i];
+            int r = prefixsum % k;
+            if (r < 0) {
+                r = r + k;
+            }
+            if (map.containsKey(r)) {
+                count += map.get(r);
+            }
+            map.put(r, map.getOrDefault(r, 0) + 1);
+        }
+        return count;
+
+    }
+}
