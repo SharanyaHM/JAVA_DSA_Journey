d2f6aca1c5fefcd22207ca1fc219a94d3adb1524
SharanyaHM <sharanyahmgowda@gmail.com>
2026-07-15 13:35:18 +0530

    Solution for container with most water using two pointers

---
+++ 01-Arrays/LC011_ContainerWithMostWater.java
@@ -0,0 +1,27 @@
+// LC11 - Container With Most Water
+// Link: https://leetcode.com/problems/container-with-most-water/
+// Time: O(n) | Space: O(1)
+ 
+public class LC011_ContainerWithMostWater {
+   public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int MaxArea = 0;
+        while (i < j) {
+            int Area = Integer.min(height[i], height[j]) * (j - i);
+            if (Area > MaxArea) {
+                MaxArea = Area;
+            }
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+
+        }
+        return MaxArea;
+
+    }
+
+    
+}
