1ecb1e6c6052b5240764d0014890e3d4a0d25b2c
SharanyaHM <sharanyahmgowda@gmail.com>
2026-07-15 13:37:50 +0530

    Solution for SortColors using Dutch National Flag Algo

---
+++ 01-Arrays/LC075_SortColors.java
@@ -0,0 +1,31 @@
+// LC75 - Sort Colors
+// Link: https://leetcode.com/problems/sort-colors/
+// Time: O(n) | Space: O(1)
+
+public class LC075_SortColors {
+
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+
+    }
+}
